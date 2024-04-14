87bcb97c86334a4da592e8f1ec469a1645e94646
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-04-14 11:20:42 +0200

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+require('dotenv').config(); // Load variables from .env
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-SvjLpBVuCjovDb8FQ3mC4THy22khcig",
-  authDomain: "port-d64f2.firebaseapp.com",
-  projectId: "port-d64f2",
-  storageBucket: "port-d64f2.appspot.com",
-  messagingSenderId: "320967397995",
-  appId: "1:320967397995:web:8bf6db3610648faa58572e",
-  measurementId: "G-GK66WZ6Y20"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
