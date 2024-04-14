b9a4fee0a47b8dedcd2d5811f762c1540cd491da
jamesdanielwhitford <70632508+jamesdanielwhitford@users.noreply.github.com>
2024-04-14 12:12:36 +0200

    Firebase envs

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,13 @@
 // Initialize Firebase (make sure to replace the placeholders with your own configuration values)
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
-};
+    apiKey: firebase.env.FIREBASE_API_KEY,
+    authDomain: firebase.env.FIREBASE_AUTH_DOMAIN,
+    projectId: firebase.env.FIREBASE_PROJECT_ID,
+    storageBucket: firebase.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: firebase.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: firebase.env.FIREBASE_APP_ID,
+    measurementId: firebase.env.FIREBASE_MEASUREMENT_ID
+  };
   firebase.initializeApp(firebaseConfig);
   
   // Get references to Firebase services
